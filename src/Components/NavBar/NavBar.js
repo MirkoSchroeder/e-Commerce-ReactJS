8283d76dcc003908f94748a5db648fd43d6f7738
Mirko Schroeder <mirschroeder@hotmail.com>
2022-06-20 01:19:45 -0300

    agregado menu eCommerce

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,18 +7,29 @@ import "./NavBar.css"
 
 const Header = () => {
     return (
-        <header>
-            <img src={logo} alt="" />
+        <header class="main-header">
+            <div class="areas">
+                <div class="area-logo">
+                    <img class="main-logo" src={logo} alt="" />
+                </div>
+                <div class="area-search">
+                    <input class="input" type="text" placeholder="Buscar" />
+                </div>
+                <div>
+                    <p class="paragraph-header">Bienvenido/a</p>
+                </div>
+                <div class="area-items">
+                    <ShoppingCartIcon fontSize="large" sx={{ color: grey[100] }} />
+                </div>
+            </div>
 
-            <nav class="nav">
-                <ul>
-                    <li class="navItem"><a href="#">Notebooks</a></li>
+            <nav>
+                <ul class="navContainer">
+                    <li class="navItem"><a href="">Notebooks</a></li>
                     <li class="navItem"><a href="">Monitores</a></li>
                     <li class="navItem"><a href="">Placas de video</a></li>
                 </ul>
             </nav>
-
-            <ShoppingCartIcon fontSize="large" sx={{ color: grey[100] }} />
 
         </header>
     )
