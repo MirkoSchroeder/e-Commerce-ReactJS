ac83eea6a1368609e153168bc1328553a2b2b68a
Mirko Schroeder <mirschroeder@hotmail.com>
2022-06-19 21:00:27 -0300

    agregado menu eCommerce

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../../assets/logo.png"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { grey } from '@mui/material/colors';
+
+import "./NavBar.css"
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt="" />
+
+            <nav class="nav">
+                <ul>
+                    <li class="navItem"><a href="#">Notebooks</a></li>
+                    <li class="navItem"><a href="">Monitores</a></li>
+                    <li class="navItem"><a href="">Placas de video</a></li>
+                </ul>
+            </nav>
+
+            <ShoppingCartIcon fontSize="large" sx={{ color: grey[100] }} />
+
+        </header>
+    )
+}
+
+export default Header
+
+/* if(condicion){
+    codigo1
+}else{
+    codigo2
+}
+
+color = condicion ? "white" : "black" */
